5b056d52c7f97a0d8967e77d4ece16fd08a965dd
Lohan Alexandre <Lohan_toniatti@hotmail.com>
2023-12-02 20:48:09 -0300

    Versão 1.5: Testes automatizados implementados sem perca da entrada original!

--- Ordenacao_Topologica/br/unirio/GrafoRandomico.java
+++ Ordenacao_Topologica/br/unirio/GrafoRandomico.java
@@ -3,44 +3,53 @@ package br.unirio;
 
 import java.util.Random;
 
-public class GrafoRandomico {
+public class GrafoRandomico implements Cloneable {
 
     private int numVertices;
     private int numArestas;
     private OrdenacaoTopologica listaAdjacencia;
     private Random random = new Random();
 
-    public GrafoRandomico(int numVertices){
+    public GrafoRandomico(int numVertices) {
         this.numVertices = numVertices;
         this.numArestas = calculaQuantidadeAresta();
         this.listaAdjacencia = new OrdenacaoTopologica();
 
-        for(int i = 0; i < numArestas; i++){
+        for (int i = 0; i < numArestas; i++) {
             int v = random.nextInt(numVertices);
             int a = random.nextInt(numVertices);
-            if(v == a || !listaAdjacencia.geraArestaPossivel(v, a, numVertices)){
+            if (v == a || !listaAdjacencia.geraArestaPossivel(v, a, numVertices)) {
                 i--;
-            }
-            else
+            } else
                 listaAdjacencia.adicionarAresta(v, a);
 
         }
 
 
-
     }
 
-    private int calculaQuantidadeAresta(){
-        int max = (numVertices*(numVertices-1))/2;
+    private int calculaQuantidadeAresta() {
+        int max = (numVertices * (numVertices - 1)) / 2;
         int min = numVertices;
         int intervalo = max - min + 1;
-        return (int)(Math.random() * intervalo) + min;
+        return (int) (Math.random() * intervalo) + min;
     }
 
-    public boolean executa(){
+    public boolean executa() {
         return listaAdjacencia.executa();
     }
 
+    @Override
+    public GrafoRandomico clone() {
+        try {
+            GrafoRandomico clone = (GrafoRandomico) super.clone();
+            clone.listaAdjacencia = this.listaAdjacencia.reconstroi(); // Deep clone OrdenacaoTopologica
+            // Additional deep cloning if needed
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();  // This should never happen
+        }
+    }
 
 
 }
